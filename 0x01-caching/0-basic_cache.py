1b9b6476c965eeb69b1a3204b4951d69777e1c03
Destiny-Kay <destinykevogo@gmail.com>
2024-08-05 05:23:42 +0300

    Add: Basic and FIFO cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Basic cache class.'''
+BaseCaching = __import__('base_caching.py').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''Basisc caching class'''
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
